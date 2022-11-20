7200b37eab40fdda4f41e0a4eb1ed57532983135
Simon Wick <Simon.Wick@edu.gbssg.ch>
2022-11-20 19:55:51 +0100

    General layout + sticky header

---
+++ tailwind_config.js
@@ -0,0 +1,10 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                background: '#1C1D1B',
+                accent: '#DF227A'
+            }
+        }
+    }
+}
