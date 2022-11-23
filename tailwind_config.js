63522564fa4b6912d88815b9b440c2478d84284f
Simon Wick <Simon.Wick@edu.gbssg.ch>
2022-11-23 08:55:15 +0100

    Logo rotates around center

--- tailwind_config.js
+++ tailwind_config.js
@@ -4,6 +4,10 @@ tailwind.config = {
             colors: {
                 background: '#1C1D1B',
                 accent: '#DF227A'
+            },
+            spacing: {
+                '900': '900px',
+                '600': '600px'
             }
         }
     }
