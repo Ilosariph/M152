e68d355ce05a35b4979711c88f30d924d5023544
Simon Wick <Simon.Wick@edu.gbssg.ch>
2022-12-23 14:05:46 +0100

    Remove unnecessary settings

--- menu.js
+++ menu.js
@@ -1,5 +1,5 @@
-const button = document.querySelector('#menu-button'); // Hamburger Icon
-const menu = document.querySelector('#menu'); // Menu
+const button = document.querySelector('#menu-button');
+const menu = document.querySelector('#menu');
 
 button.addEventListener('click', () => {
   menu.classList.toggle('hidden');
--- tailwind_config.js
+++ tailwind_config.js
@@ -4,10 +4,6 @@ tailwind.config = {
             colors: {
                 background: '#1C1D1B',
                 accent: '#DF227A'
-            },
-            spacing: {
-                fotodiv: 'calc(100vh - 64px)',
-                modal: '66vh'
             }
         }
     }
